f5653d5135a2cbc846a492c47036f0181660b1e4
ShubhiAgarwal09 <shubhi.ag09@gmail.com>
2023-07-04 22:55:07 +0530

    dp5

---
+++ knapsackMemoization.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+#include<vector>
+using namespace std;
+
+int solveMem(vector<int>& weight, vector<int>& value, int index, int capacity,vector<vector<int>>& dp){
+	//base case
+	if(index==0){
+		if(weight[0]<=capacity){
+			return value[0];
+		}else{
+			return 0;
+		}
+	}
+
+	if(dp[index][capacity]!=-1){
+		return dp[index][capacity];
+	}
+
+	int include=0;
+	if(weight[index]<=capacity){
+		include = value[index]+solveMem(weight,value,index-1,capacity-weight[index],dp);
+	}
+	int exclude = 0+solveMem(weight,value,index-1,capacity,dp);
+
+	dp[index][capacity] = max(include,exclude);
+	return dp[index][capacity];
+}
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	vector<vector<int>> dp(n,vector<int>(maxWeight+1,-1));
+	return solveMem(weight,value,n-1,maxWeight,dp);
+	//return solve(weight,value,n-1,maxWeight);
+	// Write your code here
+}
